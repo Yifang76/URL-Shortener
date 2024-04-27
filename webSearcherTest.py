f2ad7a0d9776360a00cfe685d7f9af5ec71f0ef1
Yifang He <105815046+Yifang76@users.noreply.github.com>
2024-04-27 11:43:23 +0100

    addedImageOption

--- webSearcherTest.py
+++ webSearcherTest.py
@@ -4,12 +4,15 @@ from websearch import WebSearch as web
 whichURL = input("Search Fake Google ")
 imageOrLink = input("Image or Website? ").upper()
 if imageOrLink == "Image" or imageOrLink == "I":
-
+  def search(whichURL):
+    for im in web(whichURL).images[:100]:
+      print(im)
 elif imageOrLink == "Website" or imageOrLink == "W":
-  
+  def search(whichURL):
+    for page in web(whichURL).pages[:100]:
+      print(page)
 while True:
-  for page in web(whichURL).pages[:100]:
-    print(page)
+  search(whichURL)
   searchAgain = input("Would you like to load more pages? ").upper()
   if searchAgain == "YES" or searchAgain == "Y":
     try:
