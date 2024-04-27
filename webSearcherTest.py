08db8694d13983e1b2ab67dafcc878cd6e7c3a0b
Yifang He <105815046+Yifang76@users.noreply.github.com>
2024-04-27 11:47:58 +0100

    createdWebSearcherFunction

--- webSearcherTest.py
+++ webSearcherTest.py
@@ -1,28 +1,29 @@
-from websearch import WebSearch as web
+def webSearcher():
+  from websearch import WebSearch as web
 
 
-whichURL = input("Search Fake Google ")
-imageOrLink = input("Image or Website? ").upper()
-if imageOrLink == "Image" or imageOrLink == "I":
-  def search(whichURL):
-    for im in web(whichURL).images[:100]:
-      print(im)
-elif imageOrLink == "Website" or imageOrLink == "W":
-  def search(whichURL):
-    for page in web(whichURL).pages[:100]:
-      print(page)
-while True:
-  search(whichURL)
-  searchAgain = input("Would you like to load more pages? ").upper()
-  if searchAgain == "YES" or searchAgain == "Y":
-    try:
-      for page in web(whichURL).next_page().pages[:100]:
+  whichURL = input("Search Fake Google ")
+  imageOrLink = input("Image or Website? ").upper()
+  if imageOrLink == "Image" or imageOrLink == "I":
+    def search(whichURL):
+      for im in web(whichURL).images[:100]:
+        print(im)
+  elif imageOrLink == "Website" or imageOrLink == "W":
+    def search(whichURL):
+      for page in web(whichURL).pages[:100]:
         print(page)
-      #broken?
-    except:
-      print("No more pages")
+  while True:
+    search(whichURL)
+    searchAgain = input("Would you like to load more pages? ").upper()
+    if searchAgain == "YES" or searchAgain == "Y":
+      try:
+        for page in web(whichURL).next_page().pages[:100]:
+          print(page)
+        #broken?
+      except:
+        print("No more pages")
+        break
+    else:
       break
-  else:
-    break
-    
+      
 
